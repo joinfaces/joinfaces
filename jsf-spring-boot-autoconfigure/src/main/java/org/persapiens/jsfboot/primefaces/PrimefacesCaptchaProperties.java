d4d681539808fa7e8bbad1df1a8a78b63914b3ce
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-05-11 13:53:51 -0300

    fix #6

---
+++ jsf-spring-boot-autoconfigure/src/main/java/org/persapiens/jsfboot/primefaces/PrimefacesCaptchaProperties.java
@@ -0,0 +1,14 @@
+package org.persapiens.jsfboot.primefaces;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "org.primefaces.component.captcha")
+public class PrimefacesCaptchaProperties {
+    
+    private String privateKey;
+    
+}
---
+++ jsf-spring-boot-autoconfigure/src/test/java/org/persapiens/jsfboot/primefaces/PrimefacesPropertiesTest.java
@@ -0,0 +1,16 @@
+package org.persapiens.jsfboot.primefaces;
+
+import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Test
+public class PrimefacesPropertiesTest {
+
+	public void testTheme() {
+		PrimefacesProperties primefacesProperties = new PrimefacesProperties();
+        primefacesProperties.setTheme("cupertino");
+
+		assertThat(primefacesProperties.getTheme()).isEqualTo("cupertino");
+	}
+
+}
