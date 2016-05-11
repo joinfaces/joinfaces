97497711b7402da1ac60cd12959e21e8487b2bb6
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-05-11 13:47:52 -0300

    fix #6

---
+++ jsf-spring-boot-autoconfigure/src/main/java/org/persapiens/jsfboot/primefaces/PrimefacesProperties.java
@@ -0,0 +1,90 @@
+package org.persapiens.jsfboot.primefaces;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Taken from http://www.primefaces.org/docs/api/5.3/constant-values.html
+ */
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "primefaces")
+public class PrimefacesProperties {
+    
+    private String privateCaptchaKey;
+    
+    private String publicCaptchaKey;
+
+	/**
+	 * Theme of the application.
+	 */
+	private String theme;
+    
+	private Mobile mobile = new Mobile(); 
+    
+    @Getter @Setter    
+    public static class Mobile {
+        /**
+         * Theme of the mobile application.
+         */
+        private String theme;
+    }
+    
+    /**
+     * Custom server url for PrimeFaces Push.
+     */
+    private String pushServerUrl;
+    
+    /**
+     * Defines ajax submit mode, full or partial.
+     */
+    private String submit;
+    
+    /**
+     * Defines orientation, ltr or rtl.
+     */
+    private String dir;
+    
+    /**
+     * When enabled, ajax updated inputs are reset.
+     */
+    private Boolean resetValues;
+    
+    /**
+     * Secret key to encrypt-decrypt value expressions exposed in rendering StreamedContents.
+     */
+    private String secret;
+    
+    /**
+     * Controls client side validation.
+     */
+    private Boolean clientSideValidation;
+    
+    /**
+     * Defines uploader mode; auto, native or commons.
+     */
+    private String uploader;
+    
+    /**
+     * Transforms bean validation metadata to html attributes.
+     */
+    private Boolean transformMetadata;
+    
+    /**
+     * Enables window scope so that widgets can be accessed 
+     * using widgetVar.method() in addition to default PF
+     * namespace approach like PF('widgetVar').method().
+     */
+    private Boolean legacyWidgetNamespace;
+
+    /**
+     * Enabled font-awesome icons.
+     */
+    private Boolean fontAwesome;
+    
+
+    private String cacheProvider;
+    
+    private String autoUpdate;
+}
