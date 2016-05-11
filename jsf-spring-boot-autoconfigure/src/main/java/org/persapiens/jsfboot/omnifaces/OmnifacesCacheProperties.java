0dea13e378b10708c9f1bb669a306ce42a7f7ba7
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-05-11 13:52:37 -0300

    fix #5

---
+++ jsf-spring-boot-autoconfigure/src/main/java/org/persapiens/jsfboot/omnifaces/OmnifacesCacheProperties.java
@@ -0,0 +1,21 @@
+package org.persapiens.jsfboot.omnifaces;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Taken from http://central.maven.org/maven2/org/omnifaces/omnifaces/1.13/omnifaces-1.13-javadoc.jar
+ */
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "")
+public class OmnifacesCacheProperties {
+    private String applicationMaxCapacity;
+            
+    private String applicationTtl;;
+            
+    private String sessionMaxCapacity;
+    
+    private String sessionTtl;
+}
---
+++ jsf-spring-boot-autoconfigure/src/test/java/org/persapiens/jsfboot/omnifaces/OmnifacesPropertiesTest.java
@@ -0,0 +1,16 @@
+package org.persapiens.jsfboot.omnifaces;
+
+import org.testng.annotations.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Test
+public class OmnifacesPropertiesTest {
+
+	public void testTheme() {
+		OmnifacesProperties omnifacesProperties = new OmnifacesProperties();
+        omnifacesProperties.setCacheProvider("myCacheProvider");
+
+		assertThat(omnifacesProperties.getCacheProvider()).isEqualTo("myCacheProvider");
+	}
+
+}
