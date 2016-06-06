fb5340b45fee82bc39e26be33b7083ec6bbc09f2
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-06-06 15:04:49 -0300

    fix #59 JsfCdiToSpringApplicationContextInitializer configures AnnotationConfigApplicationContext too

--- jsf-spring-boot-integration/src/main/java/com/github/persapiens/jsfboot/annotations/JsfCdiToSpringApplicationContextInitializer.java
+++ jsf-spring-boot-integration/src/main/java/com/github/persapiens/jsfboot/annotations/JsfCdiToSpringApplicationContextInitializer.java
@@ -3,12 +3,13 @@ package com.github.persapiens.jsfboot.annotations;
 import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 /**
  * Jsf Cdi to Spring application context initializer
  */
-public class JsfCdiToSpringApplicationContextInitializer implements ApplicationContextInitializer {
+public class JsfCdiToSpringApplicationContextInitializer implements ApplicationContextInitializer <ConfigurableApplicationContext> {
 
     /**
      * Add scope metadata resolver, bean name generator and bean factory post processor
@@ -21,16 +22,21 @@ public class JsfCdiToSpringApplicationContextInitializer implements ApplicationC
             AnnotationConfigEmbeddedWebApplicationContext cc = (AnnotationConfigEmbeddedWebApplicationContext) c;
             cc.setScopeMetadataResolver(new JsfCdiToSpringScopeMetadataResolver());
             cc.setBeanNameGenerator(new JsfCdiToSpringBeanNameGenerator());
-            cc.addBeanFactoryPostProcessor(new JsfCdiToSpringBeanFactoryPostProcessor());
         }
-        
-        if (c instanceof AnnotationConfigWebApplicationContext)
+        else if (c instanceof AnnotationConfigWebApplicationContext)
         {
             AnnotationConfigWebApplicationContext cc = (AnnotationConfigWebApplicationContext) c;
             cc.setScopeMetadataResolver(new JsfCdiToSpringScopeMetadataResolver());
             cc.setBeanNameGenerator(new JsfCdiToSpringBeanNameGenerator());
-            cc.addBeanFactoryPostProcessor(new JsfCdiToSpringBeanFactoryPostProcessor());
         }        
+        else if (c instanceof AnnotationConfigApplicationContext)
+        {
+            AnnotationConfigApplicationContext cc = (AnnotationConfigApplicationContext) c;
+            cc.setScopeMetadataResolver(new JsfCdiToSpringScopeMetadataResolver());
+            cc.setBeanNameGenerator(new JsfCdiToSpringBeanNameGenerator());            
+        }
+        
+        c.addBeanFactoryPostProcessor(new JsfCdiToSpringBeanFactoryPostProcessor());
     }
 
 }
