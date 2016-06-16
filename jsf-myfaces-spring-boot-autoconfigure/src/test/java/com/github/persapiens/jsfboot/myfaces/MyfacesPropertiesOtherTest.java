441e887e058494a17c2fa769ade5c4a7d109c068
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-06-16 14:42:52 -0300

    fix #73 improvements to MyfacesPropertiesTest

--- jsf-myfaces-spring-boot-autoconfigure/src/test/java/com/github/persapiens/jsfboot/myfaces/MyfacesPropertiesOtherTest.java
+++ jsf-myfaces-spring-boot-autoconfigure/src/test/java/com/github/persapiens/jsfboot/myfaces/MyfacesPropertiesOtherTest.java
@@ -213,7 +213,7 @@ public class MyfacesPropertiesOtherTest {
 		MyfacesProperties myfacesProperties = new MyfacesProperties();
         myfacesProperties.setInitializeAlwaysStandalone(false);
 
-		assertThat(myfacesProperties.getInitializeAlwaysStandalone()).isEqualTo("myExpressionFactory");
+		assertThat(myfacesProperties.getInitializeAlwaysStandalone()).isFalse();
 	}
 
 }
