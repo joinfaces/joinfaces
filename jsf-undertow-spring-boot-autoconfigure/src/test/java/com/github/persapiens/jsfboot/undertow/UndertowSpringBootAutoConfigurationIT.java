caa7c202fe894c6fc24df90bcc088244b015c138
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-06-18 12:18:41 -0300

    fix #102 UndertowSpringBootAutoConfigurationIT created

--- jsf-undertow-spring-boot-autoconfigure/src/test/java/com/github/persapiens/jsfboot/undertow/UndertowSpringBootAutoConfigurationIT.java
+++ jsf-undertow-spring-boot-autoconfigure/src/test/java/com/github/persapiens/jsfboot/undertow/UndertowSpringBootAutoConfigurationIT.java
@@ -2,25 +2,31 @@ package com.github.persapiens.jsfboot.undertow;
 
 import io.undertow.servlet.api.DeploymentInfo;
 import java.io.IOException;
-import org.testng.annotations.Test;
+import java.net.MalformedURLException;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.undertow.UndertowDeploymentInfoCustomizer;
+import org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServletContainerFactory;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.annotations.Test;
 
 @SpringApplicationConfiguration(classes = UndertowSpringBootAutoConfiguration.class)
 @WebAppConfiguration
 @Test
-public class JsfUndertowDeploymentInfoCustomizerIT extends AbstractTestNGSpringContextTests {
+public class UndertowSpringBootAutoConfigurationIT extends AbstractTestNGSpringContextTests {
 
     @Autowired
-    private UndertowProperties undertowProperties;
+    private UndertowSpringBootAutoConfiguration undertowSpringBootAutoConfiguration;
+    
+	public void customize() throws MalformedURLException, IOException {
+		UndertowEmbeddedServletContainerFactory factory = new UndertowEmbeddedServletContainerFactory();
+        
+        undertowSpringBootAutoConfiguration.customize(factory);
 
-	public void testGetResource() throws IOException {
-        UndertowDeploymentInfoCustomizer undertowDeploymentInfoCustomizer 
-            = new JsfUndertowDeploymentInfoCustomizer(undertowProperties);
+        UndertowDeploymentInfoCustomizer undertowDeploymentInfoCustomizer =
+            factory.getDeploymentInfoCustomizers().iterator().next();
         
         DeploymentInfo deploymentInfo = new DeploymentInfo();
         
@@ -28,5 +34,4 @@ public class JsfUndertowDeploymentInfoCustomizerIT extends AbstractTestNGSpringC
         
 		assertThat(deploymentInfo.getResourceManager().getResource("test.txt")).isNotNull();
 	}
-
 }
