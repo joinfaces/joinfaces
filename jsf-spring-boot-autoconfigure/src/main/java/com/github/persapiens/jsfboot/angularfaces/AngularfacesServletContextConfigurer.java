781a2749400d8ba088d33c2d8567ec1b3d31d265
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-06-17 21:26:22 -0300

    fix #95 adding javax.faces.FACELETS_DECORATORS configuration according to http://angularfaces.com/index.html#/step-00.html

--- jsf-spring-boot-autoconfigure/src/main/java/com/github/persapiens/jsfboot/angularfaces/AngularfacesServletContextConfigurer.java
+++ jsf-spring-boot-autoconfigure/src/main/java/com/github/persapiens/jsfboot/angularfaces/AngularfacesServletContextConfigurer.java
@@ -3,30 +3,34 @@ package com.github.persapiens.jsfboot.angularfaces;
 import javax.servlet.ServletContext;
 import lombok.Builder;
 import com.github.persapiens.jsfboot.ServletContextConfigurer;
+import de.beyondjava.angularFaces.core.tagTransformer.AngularTagDecorator;
+import javax.faces.application.ViewHandler;
 
 public class AngularfacesServletContextConfigurer extends ServletContextConfigurer {
 
     private AngularfacesProperties angularfacesProperties;
 
-    public static final String PREFFIX = "AngularFaces";
+    public static final String PREFFIX = "AngularFaces.";
 
     @Builder
     public AngularfacesServletContextConfigurer(AngularfacesProperties angularfacesProperties, ServletContext servletContext) {
-        super(servletContext, PREFFIX);
+        super(servletContext, "");
         this.angularfacesProperties = angularfacesProperties;
     }
     
     @Override
     public void configure()
     {        
-        setInitParameter("addLabels", angularfacesProperties.getAddLabels());
-        setInitParameter("addMessages", angularfacesProperties.getAddMessages());
-        setInitParameter("translation", angularfacesProperties.getTranslation());
-        setInitParameter("includeAngularJS", angularfacesProperties.getIncludeAngularJS());
-        setInitParameter("includeJQuery", angularfacesProperties.getIncludeJQuery());
-        setInitParameter("includeJQueryUI", angularfacesProperties.getIncludeJQueryUI());
-        setInitParameter("includeAngularMessages", angularfacesProperties.getIncludeAngularMessages());
-        setInitParameter("clientSideMessages", angularfacesProperties.getClientSideMessages());
-        setInitParameter("includeMainJS", angularfacesProperties.getIncludeMainJS());
+        setInitParameter(PREFFIX + "addLabels", angularfacesProperties.getAddLabels());
+        setInitParameter(PREFFIX + "addMessages", angularfacesProperties.getAddMessages());
+        setInitParameter(PREFFIX + "translation", angularfacesProperties.getTranslation());
+        setInitParameter(PREFFIX + "includeAngularJS", angularfacesProperties.getIncludeAngularJS());
+        setInitParameter(PREFFIX + "includeJQuery", angularfacesProperties.getIncludeJQuery());
+        setInitParameter(PREFFIX + "includeJQueryUI", angularfacesProperties.getIncludeJQueryUI());
+        setInitParameter(PREFFIX + "includeAngularMessages", angularfacesProperties.getIncludeAngularMessages());
+        setInitParameter(PREFFIX + "clientSideMessages", angularfacesProperties.getClientSideMessages());
+        setInitParameter(PREFFIX + "includeMainJS", angularfacesProperties.getIncludeMainJS());
+
+        setInitParameter(ViewHandler.FACELETS_DECORATORS_PARAM_NAME, AngularTagDecorator.class.getName());
     }
 }
--- jsf-spring-boot-autoconfigure/src/test/java/com/github/persapiens/jsfboot/angularfaces/AngularfacesServletContextConfigurerIT.java
+++ jsf-spring-boot-autoconfigure/src/test/java/com/github/persapiens/jsfboot/angularfaces/AngularfacesServletContextConfigurerIT.java
@@ -1,5 +1,7 @@
 package com.github.persapiens.jsfboot.angularfaces;
 
+import de.beyondjava.angularFaces.core.tagTransformer.AngularTagDecorator;
+import javax.faces.application.ViewHandler;
 import javax.servlet.ServletContext;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +29,8 @@ public class AngularfacesServletContextConfigurerIT extends AbstractTestNGSpring
         
         angularfacesServletContextConfigurer.configure();
         
-        assertThat(servletContext.getInitParameter(AngularfacesServletContextConfigurer.PREFFIX + ".addLabels")).isEqualTo("true");
+        assertThat(servletContext.getInitParameter(AngularfacesServletContextConfigurer.PREFFIX + "addLabels")).isEqualTo("true");
+        assertThat(servletContext.getInitParameter(ViewHandler.FACELETS_DECORATORS_PARAM_NAME)).isEqualTo(AngularTagDecorator.class.getName());
 	}
 
 }
