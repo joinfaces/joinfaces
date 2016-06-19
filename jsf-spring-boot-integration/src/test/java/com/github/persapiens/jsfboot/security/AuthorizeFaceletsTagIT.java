93a9785c12a3a0ac05a1d8425934d3a21909efab
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-06-19 10:03:14 -0300

    #110 some minor integration tests for AuthorizeFaceletsTag

--- jsf-spring-boot-integration/src/test/java/com/github/persapiens/jsfboot/security/AuthorizeFaceletsTagIT.java
+++ jsf-spring-boot-integration/src/test/java/com/github/persapiens/jsfboot/security/AuthorizeFaceletsTagIT.java
@@ -14,46 +14,68 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @WebAppConfiguration
 public class AuthorizeFaceletsTagIT extends JsfIT {
 
-	public void testIfAllGrantedWithOneRole() {
+	public void testIfAllGrantedEmpty() {
+		AuthorizeFaceletsTag tag = new AuthorizeFaceletsTag();
+		tag.setIfAllGranted("");
+		assertThat(tag.getAccess())
+            .isNull();
+	}
+
+	public void testIfAnyGrantedEmpty() {
+		AuthorizeFaceletsTag tag = new AuthorizeFaceletsTag();
+		tag.setIfAnyGranted("");
+		assertThat(tag.getAccess())
+            .isNull();
+	}
+
+	public void testIfNotGrantedEmpty() {
+		AuthorizeFaceletsTag tag = new AuthorizeFaceletsTag();
+		tag.setIfNotGranted("");
+		assertThat(tag.getAccess())
+            .isNull();
+	}
+
+	public void testIfAllGrantedTwoRoles() {
 		AuthorizeFaceletsTag tag = new AuthorizeFaceletsTag();
 		tag.setIfAllGranted(Roles.ROLE_A);
-		assertThat("hasRole('ROLE_A')")
-            .isEqualTo(tag.getAccess());
+		tag.setIfAllGranted(Roles.ROLE_B);
+		assertThat(tag.getAccess())
+            .isEqualTo("hasRole('ROLE_A') and hasRole('ROLE_B')");
 	}
 
 	public void testIfAllGrantedWithMultipleRoles() {
 		AuthorizeFaceletsTag tag = new AuthorizeFaceletsTag();
 		tag.setIfAllGranted("ROLE_A, ROLE_B, ROLE_C");
-		assertThat("hasRole('ROLE_A') and hasRole('ROLE_B') and hasRole('ROLE_C')")
-            .isEqualTo(tag.getAccess());            
+		assertThat(tag.getAccess())
+            .isEqualTo("hasRole('ROLE_A') and hasRole('ROLE_B') and hasRole('ROLE_C')");
 	}
 
 	public void testIfAnyGrantedWithOneRole() {
 		AuthorizeFaceletsTag tag = new AuthorizeFaceletsTag();
 		tag.setIfAnyGranted(Roles.ROLE_A);
-		assertThat("hasAnyRole('ROLE_A')")
-            .isEqualTo(tag.getAccess());
+		assertThat(tag.getAccess())
+            .isEqualTo("hasAnyRole('ROLE_A')");
 	}
 
 	public void testIfAnyGrantedWithMultipleRole() {
 		AuthorizeFaceletsTag tag = new AuthorizeFaceletsTag();
 		tag.setIfAnyGranted("ROLE_A, ROLE_B, ROLE_C");
-		assertThat("hasAnyRole('ROLE_A','ROLE_B','ROLE_C')")
-            .isEqualTo(tag.getAccess());
+		assertThat(tag.getAccess())
+            .isEqualTo("hasAnyRole('ROLE_A','ROLE_B','ROLE_C')");
 	}
 
 	public void testIfNoneGrantedWithOneRole() {
 		AuthorizeFaceletsTag tag = new AuthorizeFaceletsTag();
 		tag.setIfNotGranted(Roles.ROLE_A);
-		assertThat("!hasAnyRole('ROLE_A')")
-            .isEqualTo(tag.getAccess());
+		assertThat(tag.getAccess())
+            .isEqualTo("!hasAnyRole('ROLE_A')");
 	}
 
 	public void testIfNoneGrantedWithMultipleRole() {
 		AuthorizeFaceletsTag tag = new AuthorizeFaceletsTag();
 		tag.setIfNotGranted("ROLE_A, ROLE_B, ROLE_C");
-		assertThat("!hasAnyRole('ROLE_A','ROLE_B','ROLE_C')")
-            .isEqualTo(tag.getAccess());
+		assertThat(tag.getAccess())
+            .isEqualTo("!hasAnyRole('ROLE_A','ROLE_B','ROLE_C')");
 	}
 
 	public void testIfAllAnyNotGranted() {
@@ -61,8 +83,8 @@ public class AuthorizeFaceletsTagIT extends JsfIT {
 		tag.setIfAllGranted(Roles.ROLE_A);
 		tag.setIfAnyGranted(Roles.ROLE_B);
 		tag.setIfNotGranted(Roles.ROLE_C);
-		assertThat("hasRole('ROLE_A') and hasAnyRole('ROLE_B') and !hasAnyRole('ROLE_C')")
-            .isEqualTo(tag.getAccess());
+		assertThat(tag.getAccess())
+            .isEqualTo("hasRole('ROLE_A') and hasAnyRole('ROLE_B') and !hasAnyRole('ROLE_C')");
 	}
 
 }
