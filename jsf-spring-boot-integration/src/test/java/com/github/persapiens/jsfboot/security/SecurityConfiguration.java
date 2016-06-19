4d6a03488a6746212e73862c524bcd03aaa14a8e
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-06-19 20:53:49 -0300

    fix #110 minor changes for SecurityConfiguration. It is missing integration classes for TagHandler classes.

--- jsf-spring-boot-integration/src/test/java/com/github/persapiens/jsfboot/security/SecurityConfiguration.java
+++ jsf-spring-boot-integration/src/test/java/com/github/persapiens/jsfboot/security/SecurityConfiguration.java
@@ -1,16 +1,9 @@
 package com.github.persapiens.jsfboot.security;
 
-
-import java.util.Arrays;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Spring security configuration for tests
@@ -20,11 +13,4 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter 
 {
-
-   @Override
-   protected UserDetailsService userDetailsService() {
-      UserDetails user1 = new User("persapiens", "123", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_ADMIN"));
-      UserDetails user2 = new User("nyilmaz", "qwe", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
-      return new InMemoryUserDetailsManager(Arrays.asList(user1, user2));
-   }
 }
