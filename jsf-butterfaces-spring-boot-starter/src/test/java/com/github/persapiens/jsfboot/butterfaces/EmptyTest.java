2b916a075c6e8e2019c2a7c1078470f739364fc9
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-07-07 10:23:31 -0300

    #122 change project version to 1.7.0-SNAPSHOT fix minor import errors detected by pmd

--- jsf-butterfaces-spring-boot-starter/src/test/java/com/github/persapiens/jsfboot/butterfaces/EmptyTest.java
+++ jsf-butterfaces-spring-boot-starter/src/test/java/com/github/persapiens/jsfboot/butterfaces/EmptyTest.java
@@ -1,6 +1,5 @@
 package com.github.persapiens.jsfboot.butterfaces;
 
-import com.github.persapiens.jsfboot.butterfaces.Empty;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.testng.annotations.Test;
 
--- jsf-jetty-butterfaces-spring-boot-starter/src/test/java/com/github/persapiens/jsfboot/butterfaces/JettyEmptyTest.java
+++ jsf-jetty-butterfaces-spring-boot-starter/src/test/java/com/github/persapiens/jsfboot/butterfaces/JettyEmptyTest.java
@@ -1,6 +1,5 @@
 package com.github.persapiens.jsfboot.butterfaces;
 
-import com.github.persapiens.jsfboot.butterfaces.JettyEmpty;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.testng.annotations.Test;
 
