ce582e69128a14ae7738f9789a65ab0770a24624
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-07-09 20:57:40 -0300

    #123 jsf-jetty-spring-boot-autoconfigure classes refactored to follow code standard

--- jsf-jetty-spring-boot-autoconfigure/src/test/java/com/github/persapiens/jsfboot/jetty/JettyPropertiesIT.java
+++ jsf-jetty-spring-boot-autoconfigure/src/test/java/com/github/persapiens/jsfboot/jetty/JettyPropertiesIT.java
@@ -1,22 +1,41 @@
+/*
+ * Copyright 2016-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.persapiens.jsfboot.jetty;
 
 import org.testng.annotations.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringApplicationConfiguration(classes = JettySpringBootAutoConfiguration.class)
 @WebAppConfiguration
 @Test
 public class JettyPropertiesIT extends AbstractTestNGSpringContextTests {
 
-    @Autowired
-    private JettyProperties jettyProperties;
+	@Autowired
+	private JettyProperties jettyProperties;
 
 	public void testForceLoadConfiguration() {
-		assertThat(jettyProperties.getClassPathResource()).isEqualTo("public");
+		assertThat(this.jettyProperties.getClassPathResource())
+			.isEqualTo("public");
 	}
 
 }
