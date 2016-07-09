f64ee0a1d657198b31b2a29b444bee730faf5849
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-07-09 19:44:21 -0300

    #123 jsf-primefaces53-spring-boot-autoconfigure classes refactored to follow code standard

--- jsf-primefaces53-spring-boot-autoconfigure/src/main/java/com/github/persapiens/jsfboot/primefaces53/PrimefacesProperties.java
+++ jsf-primefaces53-spring-boot-autoconfigure/src/main/java/com/github/persapiens/jsfboot/primefaces53/PrimefacesProperties.java
@@ -1,111 +1,137 @@
+/*
+ * Copyright 2016-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.persapiens.jsfboot.primefaces53;
 
 import lombok.Getter;
 import lombok.Setter;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
+ * Configuration properties of PrimeFaces.
  * Taken from http://www.primefaces.org/docs/api/6.0/constant-values.html
+ * @author Marcelo Fernandes
  */
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "jsf.primefaces")
 public class PrimefacesProperties {
 
-    private String privateCaptchaKey;
+	private String privateCaptchaKey;
 
-    private String publicCaptchaKey;
+	private String publicCaptchaKey;
 
-    /**
-     * Theme of the application.
-     */
-    private String theme;
+	/**
+	 * Theme of the application.
+	 */
+	private String theme;
 
-    private Mobile mobile = new Mobile();
+	private Mobile mobile = new Mobile();
 
-    @Getter
-    @Setter
-    public static class Mobile {
+	/**
+	 * Custom server url for PrimeFaces Push.
+	 */
+	private String pushServerUrl;
 
-        /**
-         * Theme of the mobile application.
-         */
-        private String theme;
-    }
+	/**
+	 * Defines ajax submit mode, full or partial.
+	 */
+	private String submit;
 
-    /**
-     * Custom server url for PrimeFaces Push.
-     */
-    private String pushServerUrl;
+	/**
+	 * Defines orientation, ltr or rtl.
+	 */
+	private String dir;
 
-    /**
-     * Defines ajax submit mode, full or partial.
-     */
-    private String submit;
+	/**
+	 * When enabled, ajax updated inputs are reset.
+	 */
+	private Boolean resetValues;
 
-    /**
-     * Defines orientation, ltr or rtl.
-     */
-    private String dir;
+	/**
+	 * Secret key to encrypt-decrypt value expressions exposed in rendering
+	 * StreamedContents.
+	 */
+	private String secret;
 
-    /**
-     * When enabled, ajax updated inputs are reset.
-     */
-    private Boolean resetValues;
+	/**
+	 * Controls client side validation.
+	 */
+	private Boolean clientSideValidation;
 
-    /**
-     * Secret key to encrypt-decrypt value expressions exposed in rendering
-     * StreamedContents.
-     */
-    private String secret;
+	/**
+	 * Defines uploader mode; auto, native or commons.
+	 */
+	private String uploader;
 
-    /**
-     * Controls client side validation.
-     */
-    private Boolean clientSideValidation;
+	/**
+	 * Transforms bean validation metadata to html attributes.
+	 */
+	private Boolean transformMetadata;
 
-    /**
-     * Defines uploader mode; auto, native or commons.
-     */
-    private String uploader;
+	/**
+	 * Enables window scope so that widgets can be accessed using
+	 * widgetVar.method() in addition to default PF namespace approach like
+	 * PF('widgetVar').method().
+	 */
+	private Boolean legacyWidgetNamespace;
 
-    /**
-     * Transforms bean validation metadata to html attributes.
-     */
-    private Boolean transformMetadata;
+	/**
+	 * Enabled font-awesome icons.
+	 */
+	private Boolean fontAwesome;
 
-    /**
-     * Enables window scope so that widgets can be accessed using
-     * widgetVar.method() in addition to default PF namespace approach like
-     * PF('widgetVar').method().
-     */
-    private Boolean legacyWidgetNamespace;
+	/**
+	 * A cache store is required to use the cache component, two different
+	 * providers are supported as cache implementation; EHCache and Hazelcast.
+	 */
+	private String cacheProvider;
 
-    /**
-     * Enabled font-awesome icons.
-     */
-    private Boolean fontAwesome;
+	private String autoUpdate;
 
-    /**
-     * A cache store is required to use the cache component, two different
-     * providers are supported as cache implementation; EHCache and Hazelcast.
-     */
-    private String cacheProvider;
+	private Captcha captcha = new Captcha();
 
-    private String autoUpdate;
+	private Boolean earlyPostParamEvaluation;
 
-    private Captcha captcha = new Captcha();
+	private Boolean beanValidationDisabled;
 
-    @Getter @Setter
-    public static class Captcha {
+	private Boolean interpolateClientSideValidationMessages;
 
-        private String privateKey;
+	/**
+	 * Mobile class of theme property.
+	 */
+	@Getter
+	@Setter
+	public static class Mobile {
 
-    }
-    
-    private Boolean earlyPostParamEvaluation;
-    
-    private Boolean beanValidationDisabled;
-    
-    private Boolean interpolateClientSideValidationMessages;
+		/**
+		 * Theme of the mobile application.
+		 */
+		private String theme;
+	}
+
+	/**
+	 * Captcha class of privateKey property.
+	 */
+	@Getter
+	@Setter
+	public static class Captcha {
+
+		private String privateKey;
+
+	}
 }
