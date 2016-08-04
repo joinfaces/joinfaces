feef74766e5802f39f1947395ab3bd680bcf8b89
J. Santos <jamillo@gmail.com>
2016-08-04 19:56:39 -0300

    Richfaces autoconfigure and its tests

---
+++ jsf-spring-boot-autoconfigure/src/main/java/org/joinfaces/richfaces/RichfacesSpringBootAutoConfiguration.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2016-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.joinfaces.richfaces;
+
+import org.joinfaces.javaxfaces.JavaxFacesSpringBootAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring Boot Auto Configuration of RichFaces.
+ * @author Marcelo Fernandes
+ */
+@Configuration
+@EnableConfigurationProperties(RichfacesProperties.class)
+@AutoConfigureBefore(JavaxFacesSpringBootAutoConfiguration.class)
+@ConditionalOnWebApplication
+public class RichfacesSpringBootAutoConfiguration
+{
+
+	@Autowired
+	private RichfacesProperties richfacesProperties;
+
+	@Bean
+	public ServletContextInitializer richfacesServletContextInitializer() {
+		return new RichfacesServletContextInitializer(this.richfacesProperties);
+	}
+}
