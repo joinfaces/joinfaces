59d770eb0b5aa134ab1a376fde57307c9a354ab8
didiez <didiez@didiez>
2016-10-25 09:18:32 +0200

    Reverted formatting changes

--- jsf-spring-boot-integration/src/main/java/org/joinfaces/annotations/ViewScope.java
+++ jsf-spring-boot-integration/src/main/java/org/joinfaces/annotations/ViewScope.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.joinfaces.annotations;
 
 import java.util.Map;
@@ -26,43 +27,43 @@ import org.springframework.web.context.request.RequestContextHolder;
 
 /**
  * Implementation of view scope.
- *
  * @author Marcelo Fernandes
  */
 public class ViewScope implements Scope {
 
-    @Override
-    public Object get(String name, ObjectFactory objectFactory) {
-        Map<String, Object> viewMap = FacesContext.getCurrentInstance().getViewRoot().getViewMap();
+	@Override
+	public Object get(String name, ObjectFactory objectFactory) {
+		Map<String, Object> viewMap = FacesContext.getCurrentInstance().getViewRoot().getViewMap();
 
-        if (viewMap.containsKey(name)) {
-            return viewMap.get(name);
-        } else {
-            Object object = objectFactory.getObject();
-            viewMap.put(name, object);
+		if (viewMap.containsKey(name)) {
+			return viewMap.get(name);
+		}
+		else {
+			Object object = objectFactory.getObject();
+			viewMap.put(name, object);
 
-            return object;
-        }
-    }
+			return object;
+		}
+	}
 
-    @Override
-    public Object remove(String name) {
-        return FacesContext.getCurrentInstance().getViewRoot().getViewMap().remove(name);
-    }
+	@Override
+	public Object remove(String name) {
+		return FacesContext.getCurrentInstance().getViewRoot().getViewMap().remove(name);
+	}
 
-    @Override
-    public String getConversationId() {
-        return null;
-    }
+	@Override
+	public String getConversationId() {
+		return null;
+	}
 
-    @Override
-    public void registerDestructionCallback(String name, Runnable callback) {
-        //Not supported
-    }
+	@Override
+	public void registerDestructionCallback(String name, Runnable callback) {
+		//Not supported
+	}
 
-    @Override
-    public Object resolveContextualObject(String key) {
-        RequestAttributes attributes = RequestContextHolder.currentRequestAttributes();
-        return attributes.resolveReference(key);
-    }
+	@Override
+	public Object resolveContextualObject(String key) {
+		RequestAttributes attributes = RequestContextHolder.currentRequestAttributes();
+		return attributes.resolveReference(key);
+	}
 }
