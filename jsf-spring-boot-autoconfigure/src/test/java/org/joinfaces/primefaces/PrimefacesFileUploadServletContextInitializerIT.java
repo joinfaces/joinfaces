27ff786e6a255f8c3e8ecb9a68fcbfd11b8d4e21
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-11-06 00:14:08 -0300

    fix #202 adding tests for PrimefacesFileUploadServletContextInitializer and PrimefacesFileUploadServletContextAutoConfiguration

---
+++ jsf-spring-boot-autoconfigure/src/test/java/org/joinfaces/primefaces/PrimefacesFileUploadServletContextInitializerIT.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2016-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.joinfaces.primefaces;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.mockito.Mockito;
+
+import org.primefaces.webapp.filter.FileUploadFilter;
+
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {MockPrimefacesFileUploadAutoConfiguration.class, PrimefacesFileUploadServletContextAutoConfiguration.class, PrimefacesSpringBootAutoConfiguration.class}, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public class PrimefacesFileUploadServletContextInitializerIT {
+
+	@Autowired
+	private MultipartConfigElement multipartConfigElement;
+
+	@Autowired(required = false)
+	private FileUploadFilter fileUploadFilter;
+
+	@Autowired
+	private PrimefacesFileUploadServletContextAutoConfiguration primefacesFileUploadServletContextAutoConfiguration;
+
+	@Test
+	public void testOnStartup() throws ServletException {
+		PrimefacesFileUploadServletContextInitializer primefacesServletContextInitializer
+			= new PrimefacesFileUploadServletContextInitializer(multipartConfigElement);
+
+		MockServletRegistrationDynamic servletRegistration = new MockServletRegistrationDynamic();
+		ServletContext servletContext = Mockito.mock(ServletContext.class);
+		Mockito.when(servletContext.getServletRegistration("FacesServlet")).thenReturn(servletRegistration);
+
+		primefacesServletContextInitializer.onStartup(servletContext);
+
+		assertThat(servletRegistration.getMultipartConfig())
+			.isEqualTo(multipartConfigElement);
+	}
+
+	@Test
+	public void testOnStartup2() throws ServletException {
+		PrimefacesFileUploadServletContextInitializer primefacesServletContextInitializer
+			= new PrimefacesFileUploadServletContextInitializer(multipartConfigElement);
+
+		MockServletRegistration servletRegistration = new MockServletRegistration();
+		ServletContext servletContext = Mockito.mock(ServletContext.class);
+		Mockito.when(servletContext.getServletRegistration("FacesServlet")).thenReturn(servletRegistration);
+
+		primefacesServletContextInitializer.onStartup(servletContext);
+
+		assertThat(servletRegistration.getMultipartConfig())
+			.isNotEqualTo(multipartConfigElement);
+	}
+
+	@Test
+	public void testFileUploadFilterNull() {
+		assertThat(fileUploadFilter)
+			.isNull();
+	}
+
+	@Test(expected = NoSuchBeanDefinitionException.class)
+	public void testFileUploadFilter() throws ServletException {
+		assertThat(primefacesFileUploadServletContextAutoConfiguration.fileUploadFilter())
+			.isNotNull();
+	}
+
+}
