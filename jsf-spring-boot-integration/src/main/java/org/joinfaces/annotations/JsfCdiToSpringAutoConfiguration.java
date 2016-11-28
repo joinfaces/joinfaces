a70f6b33331a454cfccd78f5c57de226fde72b55
didiez <diegodiez.ddr@gmail.com>
2016-11-28 23:46:41 +0100

    Checkstyle fixes

--- jsf-spring-boot-integration/src/main/java/org/joinfaces/annotations/JsfCdiToSpringAutoConfiguration.java
+++ jsf-spring-boot-integration/src/main/java/org/joinfaces/annotations/JsfCdiToSpringAutoConfiguration.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2016 JoinFaces.
+ * Copyright 2016-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *	  http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.joinfaces.annotations;
 
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
@@ -22,8 +23,8 @@ import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 
 /**
- * Auto-configuration for JSF-CDI-Spring integration
- * @author Diego Díez
+ * Auto-configuration for JSF-CDI-Spring integration.
+ * @author Diego Diez
  */
 @Configuration
 public class JsfCdiToSpringAutoConfiguration {
@@ -31,7 +32,7 @@ public class JsfCdiToSpringAutoConfiguration {
 	public static final String JSF_CDI_INITIALIZER_ORDER = "jsf.cdi.initializerOrder";
 	
 	@Bean
-	public static BeanFactoryPostProcessor beanFactoryPostProcessor(Environment environment){
+	public static BeanFactoryPostProcessor beanFactoryPostProcessor(Environment environment) {
 		JsfCdiToSpringBeanFactoryPostProcessor bfpp = new JsfCdiToSpringBeanFactoryPostProcessor();
 		bfpp.setOrder(environment.getProperty(JSF_CDI_INITIALIZER_ORDER, Integer.class, Ordered.LOWEST_PRECEDENCE));
 		return bfpp;
