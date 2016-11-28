8cd958984f22319246834fd266ad8673d3e7482c
didiez <diegodiez.ddr@gmail.com>
2016-11-28 22:07:28 +0100

    Changes to allow parameterize the order for the JsfCdiToSpringBeanFactoryPostProcessor. Registered by an auto-configuration class instead of a ApplicationContextListener to be able to load the appropiate configuration value (property sources priority, sprin profiles, etc). Ordered.LOWEST_PRECEDENCE by default.

---
+++ jsf-spring-boot-integration/src/main/java/org/joinfaces/annotations/JsfCdiToSpringAutoConfiguration.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016 JoinFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.joinfaces.annotations;
+
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
+
+/**
+ * Auto-configuration for JSF-CDI-Spring integration
+ * @author Diego Díez
+ */
+@Configuration
+public class JsfCdiToSpringAutoConfiguration {
+
+	public static final String JSF_CDI_INITIALIZER_ORDER = "jsf.cdi.initializerOrder";
+	
+	@Bean
+	public static BeanFactoryPostProcessor beanFactoryPostProcessor(Environment environment){
+		JsfCdiToSpringBeanFactoryPostProcessor bfpp = new JsfCdiToSpringBeanFactoryPostProcessor();
+		bfpp.setOrder(environment.getProperty(JSF_CDI_INITIALIZER_ORDER, Integer.class, Ordered.LOWEST_PRECEDENCE));
+		return bfpp;
+	}
+
+}
