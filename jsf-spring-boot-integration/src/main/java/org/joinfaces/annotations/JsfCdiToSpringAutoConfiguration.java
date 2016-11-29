d9fd29ce8e0639c155c6af0eca23aee209035834
didiez <diegodiez.ddr@gmail.com>
2016-11-29 22:02:03 +0100

    Removed unused import

--- jsf-spring-boot-integration/src/main/java/org/joinfaces/annotations/JsfCdiToSpringAutoConfiguration.java
+++ jsf-spring-boot-integration/src/main/java/org/joinfaces/annotations/JsfCdiToSpringAutoConfiguration.java
@@ -16,7 +16,6 @@
 
 package org.joinfaces.annotations;
 
-import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
