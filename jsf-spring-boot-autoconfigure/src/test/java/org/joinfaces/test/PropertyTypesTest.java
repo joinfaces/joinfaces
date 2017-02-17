bd0ee64d34ca9037296b749f688a473899fc816c
Lars Grefer <github@larsgrefer.de>
2017-02-17 21:09:48 +0100

    fix checkstyle

--- jsf-spring-boot-autoconfigure/src/test/java/org/joinfaces/test/PropertyTypesTest.java
+++ jsf-spring-boot-autoconfigure/src/test/java/org/joinfaces/test/PropertyTypesTest.java
@@ -16,14 +16,14 @@
 
 package org.joinfaces.test;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
