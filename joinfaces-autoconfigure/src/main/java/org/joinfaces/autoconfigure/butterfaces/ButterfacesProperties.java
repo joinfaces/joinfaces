d3f974023e7648304041c5b501dc0414cd7c976b
larmic <mail@larmic.de>
2018-02-27 20:54:43 +0100

    Upgrade ButterFaces to 3.0.0

--- joinfaces-autoconfigure/src/main/java/org/joinfaces/autoconfigure/butterfaces/ButterfacesProperties.java
+++ joinfaces-autoconfigure/src/main/java/org/joinfaces/autoconfigure/butterfaces/ButterfacesProperties.java
@@ -16,9 +16,9 @@
 
 package org.joinfaces.autoconfigure.butterfaces;
 
-import org.butterfaces.resolver.WebXmlParameters;
 import lombok.Getter;
 import lombok.Setter;
+import org.butterfaces.resolver.WebXmlParameters;
 import org.joinfaces.autoconfigure.configuration.NestedProperty;
 import org.joinfaces.autoconfigure.configuration.ServletContextInitParameter;
 import org.joinfaces.autoconfigure.configuration.ServletContextInitParameterConfigurationProperties;
