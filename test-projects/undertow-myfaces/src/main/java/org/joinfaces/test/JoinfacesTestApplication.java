d4b100364f6e4108e2554a14340b4579e7c659a4
Lars Grefer <github@larsgrefer.de>
2018-03-16 23:39:25 +0100

    add Test Projects

---
+++ test-projects/undertow-myfaces/src/main/java/org/joinfaces/test/JoinfacesTestApplication.java
@@ -0,0 +1,16 @@
+package org.joinfaces.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author Lars Grefer
+ */
+@SpringBootApplication
+public class JoinfacesTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoinfacesTestApplication.class, args);
+    }
+}
