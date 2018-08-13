41d2104b81eed192eaa88fc5c365050e1dbb2472
Marcelo Romulo Fernandes <persapiens@gmail.com>
2018-08-13 11:42:30 -0300

    adding adminfaces properties configuration. waiting 1.0.0-RC19 release to final tests.

--- joinfaces-autoconfigure/src/main/java/org/joinfaces/autoconfigure/adminfaces/AdminfacesAutoConfiguration.java
+++ joinfaces-autoconfigure/src/main/java/org/joinfaces/autoconfigure/adminfaces/AdminfacesAutoConfiguration.java
@@ -19,19 +19,24 @@ package org.joinfaces.autoconfigure.adminfaces;
 import javax.faces.application.ViewExpiredException;
 import javax.persistence.OptimisticLockException;
 
+import com.github.adminfaces.template.config.AdminConfig;
+import com.github.adminfaces.template.config.ControlSidebarConfig;
 import com.github.adminfaces.template.exception.AccessDeniedException;
 import com.github.adminfaces.template.session.AdminServletContextListener;
 import com.github.adminfaces.template.session.AdminSession;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.joinfaces.autoconfigure.primefaces.Primefaces4_0Properties;
 import org.joinfaces.autoconfigure.primefaces.Primefaces5_2Properties;
 import org.joinfaces.autoconfigure.primefaces.PrimefacesAutoConfiguration;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -52,8 +57,7 @@ import org.springframework.lang.Nullable;
  */
 @Slf4j
 @Configuration
-// will adminfaces autoconfigure via application.yml ?
-//@EnableConfigurationProperties(AdminfacesProperties.class)
+@EnableConfigurationProperties(AdminfacesProperties.class)
 @ComponentScan({"com.github.adminfaces.template.bean",
 	"com.github.adminfaces.template.config",
 	"com.github.adminfaces.template.security"})
@@ -64,9 +68,14 @@ import org.springframework.lang.Nullable;
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class AdminfacesAutoConfiguration {
 
+	@Autowired
+	private AdminfacesProperties adminfacesProperties;
+
 	@Bean
 	public BeanPostProcessor adminfacesPrimeFacesPropertiesPostProcessor() {
-		return new PrimeFacesPropertiesPostProcessor();
+		PrimeFacesPropertiesPostProcessor result = new PrimeFacesPropertiesPostProcessor();
+		result.setAdminfacesProperties(this.adminfacesProperties);
+		return result;
 	}
 
 	// AdminSession does not contain @Named.
@@ -100,10 +109,14 @@ public class AdminfacesAutoConfiguration {
 
 	/**
 	 * Configures Primefaces to use admin theme.
+	 * Apply adminfaces configuration from adminfaces properties.
 	 *
 	 * @author Marcelo Fernandes
 	 */
 	static class PrimeFacesPropertiesPostProcessor implements BeanPostProcessor {
+		@Setter
+		private AdminfacesProperties adminfacesProperties;
+
 		@Override
 		public Object postProcessBeforeInitialization(@Nullable Object bean, @Nullable String beanName) throws BeansException {
 			if (bean instanceof Primefaces4_0Properties) {
@@ -116,7 +129,116 @@ public class AdminfacesAutoConfiguration {
 				log.warn("Changing primefaces fontAwesome from 'false' to 'true'.");
 				properties.setFontAwesome(true);
 			}
+			/* Waiting 1.0.0-RC19 release
+			if (bean instanceof AdminConfig) {
+				configureAdminConfig((AdminConfig) bean);
+			}
+			*/
 			return bean;
+		}
+
+		private void configureAdminConfig(AdminConfig adminConfig) {
+			if (this.adminfacesProperties.getLoginPage() != null) {
+				adminConfig.setLoginPage(this.adminfacesProperties.getLoginPage());
+			}
+			if (this.adminfacesProperties.getIndexPage() != null) {
+				adminConfig.setIndexPage(this.adminfacesProperties.getIndexPage());
+			}
+			if (this.adminfacesProperties.getDateFormat() != null) {
+				adminConfig.setDateFormat(this.adminfacesProperties.getDateFormat());
+			}
+			if (this.adminfacesProperties.getTemplatePath() != null) {
+				adminConfig.setTemplatePath(this.adminfacesProperties.getTemplatePath());
+			}
+			if (this.adminfacesProperties.getBreadcrumbSize() != null) {
+				adminConfig.setBreadCrumbMaxSize(this.adminfacesProperties.getBreadcrumbSize());
+			}
+			if (this.adminfacesProperties.getRenderMessages() != null) {
+				adminConfig.setRenderMessages(this.adminfacesProperties.getRenderMessages());
+			}
+			if (this.adminfacesProperties.getRenderAjaxStatus() != null) {
+				adminConfig.setRenderAjaxStatus(this.adminfacesProperties.getRenderAjaxStatus());
+			}
+			if (this.adminfacesProperties.getDisableFilter() != null) {
+				adminConfig.setDisableFilter(this.adminfacesProperties.getDisableFilter());
+			}
+			if (this.adminfacesProperties.getRenderBreadCrumb() != null) {
+				adminConfig.setRenderBreadCrumb(this.adminfacesProperties.getRenderBreadCrumb());
+			}
+			if (this.adminfacesProperties.getEnableSlideMenu() != null) {
+				adminConfig.setEnableSlideMenu(this.adminfacesProperties.getEnableSlideMenu());
+			}
+			if (this.adminfacesProperties.getEnableRipple() != null) {
+				adminConfig.setEnableRipple(this.adminfacesProperties.getEnableRipple());
+			}
+			if (this.adminfacesProperties.getRippleElements() != null) {
+				adminConfig.setRippleElements(this.adminfacesProperties.getRippleElements());
+			}
+			if (this.adminfacesProperties.getSkin() != null) {
+				adminConfig.setSkin(this.adminfacesProperties.getSkin());
+			}
+			if (this.adminfacesProperties.getAutoShowNavbar() != null) {
+				adminConfig.setAutoShowNavbar(this.adminfacesProperties.getAutoShowNavbar());
+			}
+			if (this.adminfacesProperties.getIgnoredResources() != null) {
+				adminConfig.setIgnoredResources(this.adminfacesProperties.getIgnoredResources());
+			}
+			if (this.adminfacesProperties.getLoadingImage() != null) {
+				adminConfig.setLoadingImage(this.adminfacesProperties.getLoadingImage());
+			}
+			if (this.adminfacesProperties.getExtensionLessUrls() != null) {
+				adminConfig.setExtensionLessUrls(this.adminfacesProperties.getExtensionLessUrls());
+			}
+			if (this.adminfacesProperties.getRenderControlSidebar() != null) {
+				adminConfig.setRenderControlSidebar(this.adminfacesProperties.getRenderControlSidebar());
+			}
+
+			Boolean showOnMobile = adminConfig.getControlSidebar().getShowOnMobile();
+			if (this.adminfacesProperties.getControlSidebar().getShowOnMobile() != null) {
+				showOnMobile = this.adminfacesProperties.getControlSidebar().getShowOnMobile();
+			}
+			Boolean fixedLayout = adminConfig.getControlSidebar().getFixedLayout();
+			if (this.adminfacesProperties.getControlSidebar().getFixedLayout() != null) {
+				fixedLayout = this.adminfacesProperties.getControlSidebar().getFixedLayout();
+			}
+			Boolean boxedLayout = adminConfig.getControlSidebar().getBoxedLayout();
+			if (this.adminfacesProperties.getControlSidebar().getBoxedLayout() != null) {
+				boxedLayout = this.adminfacesProperties.getControlSidebar().getBoxedLayout();
+			}
+			Boolean expandOnHover = adminConfig.getControlSidebar().getExpandOnHover();
+			if (this.adminfacesProperties.getControlSidebar().getExpandOnHover() != null) {
+				expandOnHover = this.adminfacesProperties.getControlSidebar().getExpandOnHover();
+			}
+			Boolean sidebarCollapsed = adminConfig.getControlSidebar().getSidebarCollapsed();
+			if (this.adminfacesProperties.getControlSidebar().getSidebarCollapsed() != null) {
+				sidebarCollapsed = this.adminfacesProperties.getControlSidebar().getSidebarCollapsed();
+			}
+			Boolean fixed = adminConfig.getControlSidebar().getFixed();
+			if (this.adminfacesProperties.getControlSidebar().getFixed() != null) {
+				fixed = this.adminfacesProperties.getControlSidebar().getFixed();
+			}
+			Boolean darkSkin = adminConfig.getControlSidebar().getDarkSkin();
+			if (this.adminfacesProperties.getControlSidebar().getDarkSkin() != null) {
+				darkSkin = this.adminfacesProperties.getControlSidebar().getDarkSkin();
+			}
+			adminConfig.setControlSidebar(new ControlSidebarConfig(showOnMobile, fixedLayout, boxedLayout, expandOnHover, sidebarCollapsed, fixed, darkSkin));
+
+			if (this.adminfacesProperties.getControlSidebar().getLeftMenuTemplate() != null) {
+				adminConfig.setLeftMenuTemplate(this.adminfacesProperties.getControlSidebar().getLeftMenuTemplate());
+			}
+
+			if (this.adminfacesProperties.getRippleMobileOnly() != null) {
+				adminConfig.setRippleMobileOnly(this.adminfacesProperties.getRippleMobileOnly());
+			}
+			if (this.adminfacesProperties.getRenderMenuSearch() != null) {
+				adminConfig.setRenderMenuSearch(this.adminfacesProperties.getRenderMenuSearch());
+			}
+			if (this.adminfacesProperties.getAutoHideMessages() != null) {
+				adminConfig.setAutoHideMessages(this.adminfacesProperties.getAutoHideMessages());
+			}
+			if (this.adminfacesProperties.getMessagesHideTimeout() != null) {
+				adminConfig.setMessagesHideTimeout(this.adminfacesProperties.getMessagesHideTimeout());
+			}
 		}
 	}
 
---
+++ joinfaces-autoconfigure/src/main/java/org/joinfaces/autoconfigure/adminfaces/AdminfacesProperties.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright 2016-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.joinfaces.autoconfigure.adminfaces;
+
+import lombok.Data;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Configuration properties of BootsFaces.
+ * <p>
+ * Taken from
+ * <ul>
+ * <li>https://github.com/adminfaces/admin-template#configuration</li>
+ * <li>https://github.com/adminfaces/admin-template/blob/master/src/main/java/com/github/adminfaces/template/config/AdminConfig.java</li>
+ * </ul>
+ *
+ * @author Marcelo Fernandes
+ */
+@Data
+@ConfigurationProperties(prefix = "jsf.adminfaces")
+public class AdminfacesProperties {
+
+	/**
+	 * login page location (relative to webapp). It you only be used if you configure Admin Session.
+	 */
+	private String loginPage;
+
+	/**
+	 * index page location. User will be redirected to it when it access app root (contextPath/).
+	 */
+	private String indexPage;
+
+	/**
+	 * Date format used in error page (500.xhtml), by default it is JVM default format.
+	 */
+	private String dateFormat;
+
+	/**
+	 * Template path.
+	 */
+	private String templatePath;
+
+	/**
+	 * Number of breadcrumbs to queue before removing the older ones.
+	 */
+	private Integer breadcrumbSize;
+
+	/**
+	 * When false, p:messages defined in admin template will not be rendered.
+	 */
+	private Boolean renderMessages;
+
+	/**
+	 * When false ajaxStatus, which triggers the loading bar on every ajax request,
+	 * will not be rendered.
+	 */
+	private Boolean renderAjaxStatus;
+
+	/**
+	 * Disables AdminFilter, responsible for redirecting user after session timeout,
+	 * sending user to logon page when it is not logged in among other things.
+	 */
+	private Boolean disableFilter;
+
+	/**
+	 * When false, the breadCrumb component, declared in admin template, will not be rendered.
+	 */
+	private Boolean renderBreadCrumb;
+
+	/**
+	 * If true will make left menu touch enable (can be closed or opened via touch).
+	 * Can be enable/disabled per page with <ui:param name="enableSlideMenu" value="false" />.
+	 */
+	private Boolean enableSlideMenu;
+
+	/**
+	 * When true it will create a wave/ripple effect on elements specified by rippleElements.
+	 */
+	private Boolean enableRipple;
+
+	/**
+	 * A list of comma separated list of (jquery) selector which elements will be
+	 * affected by ripple effect.
+	 */
+	private String rippleElements;
+
+	/**
+	 * Default template skin.
+	 */
+	private String skin;
+
+	/**
+	 * Automatic shows navbar when users scrolls page up (on small screens).
+	 * Can be enable/disabled per page with <ui:param name="autoShowNavbar" value="false" />.
+	 */
+	private Boolean autoShowNavbar;
+
+	/**
+	 * Comma separated resources (pages or urls) to be skiped by AdminFilter.
+	 * Ex: /rest, /pages/car-list. Note that by default the filter skips pages
+	 * under CONTEXT/public/ folder.
+	 */
+	private String ignoredResources;
+
+	/**
+	 * image used for the loading popup. It must be under webapp/resources/images folder.
+	 */
+	private String loadingImage;
+
+	/**
+	 * Removes extension suffix from breadCrumb links.
+	 */
+	private Boolean extensionLessUrls;
+
+	/**
+	 * When true it will activate control sidebar component.
+	 */
+	private Boolean renderControlSidebar;
+
+	/**
+	 * When true the ripple effect will be enabled only on mobile (small) screens.
+	 */
+	private Boolean rippleMobileOnly;
+
+	/**
+	 * Enables or disables menu search.
+	 */
+	private Boolean renderMenuSearch;
+
+	/**
+	 * If true PrimeFaces info messages will be hidden after a certain timeout.
+	 */
+	private Boolean autoHideMessages;
+
+	/**
+	 * Timeout to hide info messages. Note that the timeout is also composed by
+	 * configured timeout + number of words in message.
+	 */
+	private String messagesHideTimeout;
+
+	/**
+	 * When true it will activate control sidebar component.
+	 */
+	private ControlSidebar controlSidebar = new ControlSidebar();
+
+	/**
+	 * ControlSidebar properties.
+	 */
+	@Data
+	public static class ControlSidebar {
+
+		/**
+		 * When true control sidebar will be also rendered on mobile devices.
+		 */
+		private Boolean showOnMobile;
+
+		/**
+		 * Switches layout between left (default) and top menu.
+		 */
+		private Boolean leftMenuTemplate;
+
+		/**
+		 * Toggles fixed layout where navbar is fixed on the page.
+		 */
+		private Boolean fixedLayout;
+
+		/**
+		 * Boxed layout.
+		 */
+		private Boolean boxedLayout;
+
+		/**
+		 * When true left sidebar will be collapsed.
+		 */
+		private Boolean sidebarCollapsed;
+
+		/**
+		 * When true left sidebar will expand on mouse hover.
+		 */
+		private Boolean expandOnHover;
+
+		/**
+		 * When true control sidebar will be fixed on the page.
+		 */
+		private Boolean fixed;
+
+		/**
+		 * Changes control sidebar skin between dark and light.
+		 */
+		private Boolean darkSkin;
+	}
+}
