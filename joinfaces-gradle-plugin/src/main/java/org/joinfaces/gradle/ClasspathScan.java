e53897a5b148354c7c3a69fcd785bc7abeede8b6
Lars Grefer <github@larsgrefer.de>
2018-11-15 04:11:36 +0100

    Initial maven plugin implementation

--- joinfaces-gradle-plugin/src/main/java/org/joinfaces/gradle/ClasspathScan.java
+++ joinfaces-gradle-plugin/src/main/java/org/joinfaces/gradle/ClasspathScan.java
@@ -16,35 +16,18 @@
 
 package org.joinfaces.gradle;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
 
-import io.github.classgraph.AnnotationClassRef;
-import io.github.classgraph.AnnotationInfo;
-import io.github.classgraph.ClassGraph;
-import io.github.classgraph.ClassInfo;
-import io.github.classgraph.ClassInfoList;
-import io.github.classgraph.ScanResult;
 import lombok.Getter;
 import lombok.Setter;
-import org.codehaus.groovy.runtime.ResourceGroovyMethods;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.file.ConfigurableFileCollection;
 import org.gradle.api.file.DirectoryProperty;
 import org.gradle.api.tasks.Classpath;
 import org.gradle.api.tasks.InputFiles;
-import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.TaskAction;
+import org.joinfaces.tools.ClasspathScanner;
 
 /**
  * @author Lars Grefer
@@ -53,20 +36,6 @@ import org.gradle.api.tasks.TaskAction;
 @Setter
 public class ClasspathScan extends DefaultTask {
 
-	public static final String SERVLET_CONTAINER_INITIALIZER = "javax.servlet.ServletContainerInitializer";
-	public static final String HANDLES_TYPES = "javax.servlet.annotation.HandlesTypes";
-	public static final String MYFACES_ANNOTATION_PROVIDER = "org.apache.myfaces.spi.AnnotationProvider";
-	public static final List<String> MYFACES_ANNOTATIONS = Collections.unmodifiableList(Arrays.asList(
-			"javax.faces.bean.ManagedBean",
-			"javax.faces.component.FacesComponent",
-			"javax.faces.component.behavior.FacesBehavior",
-			"javax.faces.convert.FacesConverter",
-			"javax.faces.event.NamedEvent",
-			"javax.faces.render.FacesRenderer",
-			"javax.faces.render.FacesBehaviorRenderer",
-			"javax.faces.validator.FacesValidator"
-	));
-
 	@Classpath
 	@InputFiles
 	private final ConfigurableFileCollection classpath = getProject().files();
@@ -74,105 +43,13 @@ public class ClasspathScan extends DefaultTask {
 	@OutputDirectory
 	private final DirectoryProperty destinationDir = newOutputDirectory();
 
-	@Internal
-	private File getBaseDir() {
-		return destinationDir.dir("META-INF/joinfaces").get().getAsFile();
-	}
-
 	@TaskAction
 	public void scanClasses() throws IOException {
-
-		ClassGraph classGraph = new ClassGraph()
-				.enableAllInfo()
-				.enableSystemPackages()
-				.overrideClasspath(classpath);
-
-		try (ScanResult scanResult = classGraph.scan()) {
-			processHandlesTypes(scanResult);
-
-			processMyfacesAnnotationProvider(scanResult);
-		}
+		ClasspathScanner.builder()
+				.classpathRoot(getDestinationDir().getAsFile().get())
+				.classGraphConfigurer(classGraph -> classGraph.overrideClasspath(getClasspath()))
+				.build()
+				.scanClasses();
 	}
 
-	private void processMyfacesAnnotationProvider(ScanResult scanResult) throws IOException {
-		if (scanResult.getClassInfo(MYFACES_ANNOTATION_PROVIDER) == null) {
-			return;
-		}
-
-		File resultFile = new File(getBaseDir(), MYFACES_ANNOTATION_PROVIDER + ".classes");
-
-		try (PrintWriter printWriter = ResourceGroovyMethods.newPrintWriter(resultFile, "UTF-8")) {
-			for (String myfacesAnnotation : MYFACES_ANNOTATIONS) {
-
-				printWriter.print(myfacesAnnotation);
-				printWriter.print("=");
-
-				ClassInfoList classesWithAnnotation = scanResult.getClassesWithAnnotation(myfacesAnnotation);
-				printWriter.println(String.join(",", classesWithAnnotation.getNames()));
-			}
-		}
-	}
-
-	private void processHandlesTypes(ScanResult scanResult) throws IOException {
-		ClassInfoList scis = scanResult.getClassesImplementing(SERVLET_CONTAINER_INITIALIZER)
-				.filter(classInfo -> classInfo.hasAnnotation(HANDLES_TYPES));
-
-		for (ClassInfo sciClassInfo : scis) {
-			List<ClassInfo> handledTypes = resolveHandledTypes(sciClassInfo);
-
-			SortedSet<String> classes = findHandledClasses(scanResult, handledTypes);
-
-			File resultFile = new File(getBaseDir(), sciClassInfo.getName() + ".classes");
-
-			Files.write(resultFile.toPath(), classes, StandardCharsets.UTF_8);
-		}
-	}
-
-	/**
-	 * Resolves the {@link Class}[] of the {@code HandlesTypes}-annotations present on the given
-	 * {@code ServletContainerInitializer} implementation
-	 *
-	 * @param servletContainerInitializerClassInfo The {@link ClassInfo} object representing the {@code ServletContainerInitializer} implementation
-	 */
-	private List<ClassInfo> resolveHandledTypes(ClassInfo servletContainerInitializerClassInfo) {
-		AnnotationInfo handlesTypes = servletContainerInitializerClassInfo.getAnnotationInfo(HANDLES_TYPES);
-
-		Object[] value = (Object[]) handlesTypes.getParameterValues().get("value");
-
-		return Arrays.stream(value)
-				.map(AnnotationClassRef.class::cast)
-				.map(AnnotationClassRef::getClassInfo)
-				.collect(Collectors.toList());
-	}
-
-	/**
-	 * Find the classes which match the given {@code HandlesTypes} spec.
-	 *
-	 * @param scanResult   The {@link ScanResult} in which the classes are searched.
-	 * @param handledTypes The filter spec as found in the {@code HandlesTypes} annotation.
-	 * @return The {@link SortedSet set} of classes which implement, extend or are annotated with the given classes.
-	 */
-	private SortedSet<String> findHandledClasses(ScanResult scanResult, List<ClassInfo> handledTypes) {
-
-		SortedSet<String> classes = new TreeSet<>(String::compareTo);
-
-		handledTypes.forEach(handledType -> {
-			String name = handledType.getName();
-
-			if (handledType.isAnnotation()) {
-				classes.addAll(scanResult.getClassesWithAnnotation(name).getNames());
-				classes.addAll(scanResult.getClassesWithMethodAnnotation(name).getNames());
-				classes.addAll(scanResult.getClassesWithFieldAnnotation(name).getNames());
-			}
-			else if (handledType.isInterface()) {
-				classes.addAll(scanResult.getClassesImplementing(name).getNames());
-			}
-			else {
-				classes.addAll(scanResult.getSubclasses(name).getNames());
-			}
-
-		});
-
-		return classes;
-	}
 }
---
+++ joinfaces-maven-plugin/src/main/java/org/joinfaces/maven/ClasspathScanMojo.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2016-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.joinfaces.maven;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.joinfaces.tools.ClasspathScanner;
+
+@Mojo(
+		name = "classpath-scan",
+		defaultPhase = LifecyclePhase.GENERATE_RESOURCES,
+		requiresDependencyCollection = ResolutionScope.COMPILE_PLUS_RUNTIME
+)
+public class ClasspathScanMojo extends AbstractMojo {
+
+	@Parameter(defaultValue = "${project}", readonly = true, required = true)
+	private MavenProject project;
+
+	@Parameter(defaultValue = "${project.build.outputDirectory}")
+	private File outputDirectory;
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+
+		Set<String> classpath = new HashSet<>();
+
+		try {
+			classpath.addAll(project.getRuntimeClasspathElements());
+			classpath.addAll(project.getCompileClasspathElements());
+		}
+		catch (DependencyResolutionRequiredException e) {
+			throw new MojoExecutionException("classpath not present", e);
+		}
+
+		try {
+			ClasspathScanner.builder()
+					.classpathRoot(outputDirectory)
+					.classGraphConfigurer(classGraph -> classGraph.overrideClasspath(classpath))
+					.build()
+					.scanClasses();
+		}
+		catch (IOException e) {
+			throw new MojoFailureException("Classpath scan failed", e);
+		}
+	}
+}
