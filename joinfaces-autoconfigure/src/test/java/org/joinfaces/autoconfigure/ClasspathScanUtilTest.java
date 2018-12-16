a710ebe186f7f7fc74e298324a49aeca68ccb9ef
Marcelo Romulo Fernandes <persapiens@gmail.com>
2018-12-15 23:37:36 -0300

    fix checkstyle of tests

--- joinfaces-autoconfigure/src/test/java/org/joinfaces/autoconfigure/ClasspathScanUtilTest.java
+++ joinfaces-autoconfigure/src/test/java/org/joinfaces/autoconfigure/ClasspathScanUtilTest.java
@@ -22,7 +22,6 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ClasspathScanUtilTest {
 
