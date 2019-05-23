3775061d9f8a76c6b683716d62d9a299bb3e094f
Lars Grefer <github@larsgrefer.de>
2019-05-23 02:03:24 +0200

    Don't test against Gradle 4.x anymore

--- joinfaces-tools/joinfaces-gradle-plugin/src/test/java/org/joinfaces/gradle/JoinfacesPluginIT.java
+++ joinfaces-tools/joinfaces-gradle-plugin/src/test/java/org/joinfaces/gradle/JoinfacesPluginIT.java
@@ -40,7 +40,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class JoinfacesPluginIT {
 
 	@ParameterizedTest
-	@ValueSource(strings = {"4.8", "4.10.3", "5.1.1"})
+	@ValueSource(strings = {"5.1.1", "5.4.1"})
 	public void build(String gradleVersion, @TempDir Path projectDir) throws IOException {
 		Files.write(projectDir.resolve("settings.gradle"), Collections.singleton("rootProject.name = 'dummy'"));
 
