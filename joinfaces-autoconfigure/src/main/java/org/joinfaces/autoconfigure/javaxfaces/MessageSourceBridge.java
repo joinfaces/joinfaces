b0b3d213ed30e244aade374dcadb92adc6ce1e33
Lars Grefer <github@larsgrefer.de>
2019-08-17 02:18:58 +0200

    Improve Spring MessageSource support
    fixes #720

---
+++ joinfaces-autoconfigure/src/main/java/org/joinfaces/autoconfigure/javaxfaces/MessageSourceBridge.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2016-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.joinfaces.autoconfigure.javaxfaces;
+
+import java.util.AbstractMap;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.lang.NonNull;
+
+/**
+ * This class wraps an {@link org.springframework.context.MessageSource MessageSource} and exposes it through the
+ * {@link java.util.Map#get(Object) Map API} so it can be easily accessed from JSF.
+ *
+ * @author Lars Grefer
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageSourceBridge extends AbstractMap<String, String> {
+
+	@Setter
+	private MessageSourceAccessor messageSourceAccessor;
+
+	public MessageSourceBridge(MessageSource messageSource) {
+		this.messageSourceAccessor = new MessageSourceAccessor(messageSource);
+	}
+
+	@Override
+	public String get(Object key) {
+		return this.messageSourceAccessor.getMessage((String) key);
+	}
+
+	@Override
+	@NonNull
+	public Set<Entry<String, String>> entrySet() {
+		throw new UnsupportedOperationException();
+	}
+}
