b42ab57b58a0232cd8fdcf5c53abd2905fb1d9a6
Lars Grefer <github@larsgrefer.de>
2019-10-27 00:21:23 +0200

    fix Checkstyle

--- joinfaces-autoconfigure/src/test/java/org/joinfaces/autoconfigure/session/SpringSessionFixAutoConfigurationTest.java
+++ joinfaces-autoconfigure/src/test/java/org/joinfaces/autoconfigure/session/SpringSessionFixAutoConfigurationTest.java
@@ -21,15 +21,12 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.Session;
 import org.springframework.session.web.http.SessionRepositoryFilter;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
+import static org.mockito.BDDMockito.mock;
 
 class SpringSessionFixAutoConfigurationTest {
 
