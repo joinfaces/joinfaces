8ac89e9ead5627251e81ea2e50ad3b224235de00
Lars Grefer <github@larsgrefer.de>
2022-02-25 01:24:19 +0100

    Update Spring Security Configuration

--- test-projects/src/main/java/org/joinfaces/test/SecurityConfiguration.java
+++ test-projects/src/main/java/org/joinfaces/test/SecurityConfiguration.java
@@ -16,32 +16,29 @@
 
 package org.joinfaces.test;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-	private static final Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic();
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.httpBasic().and().build();
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+		return auth.inMemoryAuthentication()
 			.withUser("admin").password("{noop}admin").roles("admin").and()
-			.withUser("user").password("{noop}user").roles("user");
+			.withUser("user").password("{noop}user").roles("user").and()
+				.and().build();
 	}
 
 	@Bean
