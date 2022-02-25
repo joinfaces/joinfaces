868475548b7cf0afa01879446a9cef73c159538d
Lars Grefer <github@larsgrefer.de>
2022-02-25 01:01:35 +0100

    fix checkstyle

--- joinfaces-autoconfigure/src/main/java/org/joinfaces/autoconfigure/JoinfacesAutoConfiguration.java
+++ joinfaces-autoconfigure/src/main/java/org/joinfaces/autoconfigure/JoinfacesAutoConfiguration.java
@@ -17,7 +17,6 @@
 package org.joinfaces.autoconfigure;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.web.servlet.ConditionalOnMissingFilterBean;
--- joinfaces-autoconfigure/src/main/java/org/joinfaces/autoconfigure/scopemapping/CdiScopeAnnotationsAutoConfiguration.java
+++ joinfaces-autoconfigure/src/main/java/org/joinfaces/autoconfigure/scopemapping/CdiScopeAnnotationsAutoConfiguration.java
@@ -27,7 +27,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.web.context.WebApplicationContext;
